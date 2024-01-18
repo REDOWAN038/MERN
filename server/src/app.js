a8debae99dbd35688e8bfd13bea01d6f3d5fcdf0
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-01-18 19:34:10 +0600

    handling error

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const createError = require('http-errors')
 const morgan = require("morgan")
 const app = express()
 
@@ -11,6 +12,20 @@ app.get("/test",(req,res)=>{
     res.status(200).json({
         message : "welcome to the server"
     })
+})
+
+// handling client error
+app.use((req,res,next)=>{
+	createError(404, "route not found")
+	next()
+})
+
+// handling server error
+app.use((err,req,res,next)=>{
+	return res.status(err.status || 500).json({
+		success : false,
+		message : err.message
+	})
 })
 
 module.exports = app
